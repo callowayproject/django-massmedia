6e8fb4bb9828570902eedd325cfa9ea0d8c43f7b
Corey Oordt <coordt@washingtontimes.com>
2010-01-29 07:36:05 -0500

    Added a Metadata type to help with handling metadata, and added some custom JSON codecs to handle dates and other metadata things.

--- massmedia/fields.py
+++ massmedia/fields.py
@@ -1,49 +1,121 @@
-
 from django.db import models
+from django.utils.simplejson import dumps, loads
+from django.utils import simplejson
 
-#try:
-#    from cPickle import dumps, loads
-#except ImportError:
-#    from pickle import dumps, loads
-    
-from django.utils.simplejson import dumps, loads    
-
-# class SerializedObject(str):
-#     """A subclass of string so it can be told whether a string is
-#        a pickled object or not (if the object is an instance of this class
-#        then it must [well, should] be a pickled one)."""
-#     pass
+import datetime, time, re
 
 class SerializedObjectField(models.TextField):
     __metaclass__ = models.SubfieldBase
     
-    def to_python(self, value):
-        # if isinstance(value, SerializedObject):
-        #     # If the value is a definite pickle; and an error is raised in de-pickling
-        #     # it should be allowed to propogate.
-        #     return loads(str(value))
-        # else:
-        try:
-            return loads(str(value))
-        except:
-                # If an error was raised, just return the plain value
-            return value
-
-    def get_db_prep_save(self, value):
-            if value is not None:# and not isinstance(value, SerializedObject):
-                value = dumps(value)
-            return str(value)
+    def __init__(self, *args, **kwargs):
+        self.decoder = kwargs.pop('decoder', None)
+        self.encoder = kwargs.pop('encoder', None)
+        super(SerializedObjectField, self).__init__(*args, **kwargs)
     
-    def get_internal_type(self): 
-            return 'TextField'
+    def to_python(self, value):
+        try:
+            if self.decoder:
+                return loads(str(value), cls=self.decoder)
+            else:
+                return loads(str(value))
+        except Exception, e:
+            # If an error was raised, just return the plain value
+            return value
+    
+    def get_db_prep_save(self, value):
+        if value is not None:# and not isinstance(value, SerializedObject):
+            if self.encoder:
+                value = self.encoder().encode(value)
+            else:
+                value = dumps(value)
+        return str(value)
+    
+    def get_internal_type(self):
+        return 'TextField'
     
     def get_db_prep_lookup(self, lookup_type, value):
-            if lookup_type == 'exact':
-                value = self.get_db_prep_save(value)
-                return super(SerializedObjectField, self).get_db_prep_lookup(lookup_type, value)
-            elif lookup_type == 'in':
-                value = [self.get_db_prep_save(v) for v in value]
-                return super(SerializedObjectField, self).get_db_prep_lookup(lookup_type, value)
-            else:
-                raise TypeError('Lookup type %s is not supported.' % lookup_type)
+        if lookup_type == 'exact':
+            value = self.get_db_prep_save(value)
+            return super(SerializedObjectField, self).get_db_prep_lookup(lookup_type, value)
+        elif lookup_type == 'in':
+            value = [self.get_db_prep_save(v) for v in value]
+            return super(SerializedObjectField, self).get_db_prep_lookup(lookup_type, value)
+        else:
+            raise TypeError('Lookup type %s is not supported.' % lookup_type)
 
+class Metadata():
+    _data = {}
+    def __init__(self, initial={}, **kwargs):
+        self._data = {}
+        for key, value in initial.items():
+            self._data[str(key)] = value
+    
+    def __getitem__(self, name):
+        if name in self._data.keys():
+            return self._data[str(name)]
+        else:
+            return None
+    
+    def __setitem__(self, name, value):
+        if name == '_data':
+            self._data = value.copy()
+        else:
+            self._data[str(name)] = value
+    
+    def iterkeys(self):
+        return self._data.__iter__()
+    
+    def keys(self):
+        return self._data.keys()
+    
+    def values(self):
+        return self._data.values()
+    
+    def has_key(self, key):
+        return self._data.has_key(key)
+    
+    def __contains__(self, value):
+        return value in self._data
+    
+    def items(self):
+        return self._data.items()
+    
+    def as_json(self):
+        result = {}
+        for key, value in self._data.items():
+            if isinstance(value, datetime.datetime):
+                result[key] =  'new Date(Date.UTC(%d,%d,%d,%d,%d,%d))'%(value.year, value.month, value.day,value.hour,value.minute,value.second)
+            elif isinstance(value, datetime.date):
+                result[key] = 'new Date(Date.UTC(%d,%d,%d))'%(value.year, value.month, value.day)
+            elif isinstance(value, time.struct_time):
+                result[key] =  'new Date(Date.UTC(%d,%d,%d,%d,%d,%d))'%(value.tm_year, value.tm_mon, value.tm_mday, value.tm_hour, value.tm_min, value.tm_sec)
+            else:
+                result[key] = value
+        return result
+    
+    def from_json(self, json_str):
+        result = simplejson.loads(json_str)
+        dt_regex = re.compile(r'^new\sDate\(Date\.UTC\((.*?)\)\)')
+        for key, value in result.items():
+            if isinstance(value, basestring):
+                m = dt_regex.match(value)
+                if m:
+                    result[key] = datetime.datetime(*(simplejson.loads('[%s]'%m.group(1))))
+        self._data.update(result)
+    
+    def __str___(self):
+        return self.as_json()
+
+
+class MetadataJSONEncoder(simplejson.JSONEncoder):
+    def default(self, obj):
+        if not isinstance(obj, Metadata):
+            return super(MetadataJSONEncoder, self).default(obj)
+        return obj.as_json()
+
+class MetadataJSONDecoder(simplejson.JSONDecoder):
+    def decode(self, json_str):
+        md = Metadata()
+        md.from_json(json_str)
+        return md
+    
