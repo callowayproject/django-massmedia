ee1ff504abb42024385d37885b90c876acc2a098
Corey Oordt <coordt@washingtontimes.com>
2010-04-06 20:37:24 -0400

    Re-did the setup.py script to handle the dependencies properly

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
+import massmedia
+
+try:
+    long_desc = open('README').read()
+except OSError:
+    long_desc = ''
+
+try:
+    reqs = open('requirements.txt').read()
+except OSError:
+    reqs = ''
 
 setup(
     name = "django-massmedia",
-    version = '0.1',
+    version = massmedia.get_version(),
     url = 'http://opensource.washingtontimes.com/projects/django-massmedia/',
     author = 'Justin Quick',
     description = 'Allows for site staff can upload and edit the media files through the site, and the filesystem is maintained in the background.',
-    packages = ['massmedia']
+    long_description = long_desc,
+    packages = ['massmedia'],
+    include_package_data=True,
+    install_requires = reqs,
 )
