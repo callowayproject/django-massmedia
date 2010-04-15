15347e78bc066f1897fb292256adda0f9e2b28a8
Corey Oordt <coordt@washingtontimes.com>
2010-04-15 07:21:47 -0400

    Refactored the views code to be more generic in some cases

--- massmedia/views.py
+++ massmedia/views.py
@@ -1,12 +1,10 @@
 from massmedia import models
-from massmedia.templatetags.media_widgets import show_media
-from tagging.models import TaggedItem,Tag
 from django.template import RequestContext
 from django.shortcuts import render_to_response,get_object_or_404
-from django.conf import settings
+from django.core.xheaders import populate_xheaders
 from django.http import Http404,HttpResponse
 from django.contrib.contenttypes.models import ContentType
-from django.core.serializers import serialize
+from django.core.exceptions import ObjectDoesNotExist
 
 def widget(request, id, type):
     try:
@@ -22,45 +20,46 @@ def widget(request, id, type):
     except model.DoesNotExist:
         return HttpResponse('%s #%s not found'%(type,id))
 
-def list_by_type(request,type):
-    try:
-        assert hasattr(models,type.capitalize())
-        model = getattr(models,type.capitalize())
-    except AssertionError:
-        raise Http404
-    return render_to_response('massmedia/list.html',{
-        'objects': model.objects.filter(
-            public=True,
-            sites__id__exact=settings.SITE_ID
-        )
-    }, context_instance=RequestContext(request))
 
-def list_by_collection(request,id):
+def list_by_collection_by_type(request, slug, type):
     return render_to_response('massmedia/list.html',{
-        'objects': [x.content_object for x in models.CollectionRelation.objects.filter(
-            collection=get_object_or_404(models.Collection, pk=id),
-            collection__public=True,
-            collection__sites__id__exact=settings.SITE_ID
-        )]
-    }, context_instance=RequestContext(request))
-
-def list_by_collection_by_type(request,id,type):
-    return render_to_response('massmedia/list.html',{
-        'objects': [x.content_object for x in models.CollectionRelation.objects.filter(
-            collection=get_object_or_404(models.Collection, pk=id),
-            collection__public=True,
-            collection__sites__id__exact=settings.SITE_ID,
+        'objects': [x.content_object for x in models.CollectionRelation.objects.public(
+            collection=get_object_or_404(models.Collection, slug=slug),
             content_type=ContentType.objects.get(name=type),
         )]
     }, context_instance=RequestContext(request))
+
+
+def mediatype_detail(request, queryset, object_id=None, slug=None,
+            slug_field='slug', template_name=None, template_name_field=None,
+            template_loader=None, extra_context=None,
+            context_processors=None, template_object_name='object',
+            mimetype=None):
     
-def list(request):
-    return render_to_response('massmedia/list_types.html',{
-        'collections':models.Collection.objects.filter(
-            public=True,
-            sites__id__exact=settings.SITE_ID
-        )
-    }, context_instance=RequestContext(request))
+    if extra_context is None: extra_context = {}
+    model = queryset.model
+    if object_id:
+        queryset = queryset.filter(pk=object_id)
+    elif slug and slug_field:
+        queryset = queryset.filter(**{slug_field: slug})
+    else:
+        raise AttributeError("Generic media detail view must be called with either an object_id or a slug/slug_field.")
+    try:
+        obj = queryset.get()
+    except ObjectDoesNotExist:
+        raise Http404("No %s found matching the query" % (model._meta.verbose_name))
+    t = obj.get_template()
+    c = RequestContext(request, {
+        'media': obj,
+    }, context_processors)
+    for key, value in extra_context.items():
+        if callable(value):
+            c[key] = value()
+        else:
+            c[key] = value
+    response = HttpResponse(t.render(c), mimetype=mimetype)
+    populate_xheaders(request, response, model, getattr(obj, obj._meta.pk.name))
+    return response
     
 def grab_categorized(request):
     videos = models.GrabVideo.objects.filter(public=True)
