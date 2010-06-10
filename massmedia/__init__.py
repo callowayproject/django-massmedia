41b68beed248e52a6ffa14300dffb4f2ac9d4f84
Corey Oordt <coordt@washingtontimes.com>
2010-06-10 13:08:03 -0400

    bumped the version for testing

--- massmedia/__init__.py
+++ massmedia/__init__.py
@@ -1,9 +1,9 @@
 __version_info__ = {
     'major': 0,
     'minor': 5,
-    'micro': 3,
-    'releaselevel': 'final',
-    'serial': 0
+    'micro': 4,
+    'releaselevel': 'beta',
+    'serial': 1
 }
 
 def get_version():
