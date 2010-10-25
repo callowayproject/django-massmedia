c30240947d786da9be9021d4a67d3bd31af51476
Justin Quick <justquick@gmail.com>
2010-10-25 12:32:05 -0400

    repackage and minor version bump

--- setup.py
+++ setup.py
@@ -12,17 +12,14 @@ except (IOError, OSError):
     reqs = ''
 
 setup(
-    name = "massmedia",
+    name = "django-massmedia",
     version = massmedia.get_version(),
-    url = 'http://opensource.washingtontimes.com/projects/django-massmedia/',
+    url = 'http://github.com/washingtontimes/django-massmedia',
     author = 'Justin Quick',
     author_email = 'jquick@washingtontimes.com',
     description = 'Allows for site staff can upload and edit the media files through the site, and the filesystem is maintained in the background.',
     long_description = long_desc,
-    packages = ['massmedia'],
+    packages = ['massmedia','massmedia.templatetags','massmedia.management','massmedia.management.commands'],
     include_package_data=True,
     install_requires = reqs,
-    dependency_links=[
-        'http://opensource.washingtontimes.com/static/dist/IPTCInfo-1.9.5-1.tar.gz#md5=4d08413881d9bdb3c9c9466bb2bee7a4',
-    ]
 )
